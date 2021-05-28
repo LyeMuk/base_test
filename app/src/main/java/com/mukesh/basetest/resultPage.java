8951cbfe635a2be6b23e22497b8b2b96a591040b
LyeMuk <mukesh2337.k@gmail.com>
2021-05-28 23:26:17 +0530

    second_day_on_this

---
+++ app/src/main/java/com/mukesh/basetest/resultPage.java
@@ -0,0 +1,14 @@
+package com.mukesh.basetest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class resultPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_page);
+    }
+}
